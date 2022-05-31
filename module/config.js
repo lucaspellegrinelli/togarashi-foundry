b6b69070fffeebc17424872e63604b4d0f337e1d
Jvccampos <jok772010@gmail.com>
2022-05-31 11:36:26 -0300

    Armas e Auras Adicionadas

--- module/config.js
+++ module/config.js
@@ -2,7 +2,23 @@ export const togarashi = {};
 
 togarashi.weaponTypes = {
     dagger: "togarashi.weaponTypes.dagger",
-    sword: "togarashi.weaponTypes.sword"
+    sword: "togarashi.weaponTypes.sword",
+    rapier: "togarashi.weaponTypes.rapier",
+    greatsword: "togarashi.weaponTypes.greatsword",
+    staff: "togarashi.weaponTypes.staff",
+    spear: "togarashi.weaponTypes.spear",
+    halberd: "togarashi.weaponTypes.halberd",
+    big_hammer: "togarashi.weaponTypes.big_hammer",
+    small_hammer: "togarashi.weaponTypes.small_hammer",
+    big_axe: "togarashi.weaponTypes.big_axe",
+    small_axe: "togarashi.weaponTypes.small_axe",
+    gauntlet:"togarashi.weaponTypes.gauntlet",
+    katar:"togarashi.weaponTypes.katar",
+    chakram:"togarashi.weaponTypes.chakram",
+    impact_chains:"togarashi.weaponTypes.impact_chains",
+    slashing_chain:"togarashi.weaponTypes.slashing_chain",
+    shield:"togarashi.weaponTypes.shield"
+    
 };
 
 togarashi.armorTypes = {
@@ -12,12 +28,44 @@ togarashi.armorTypes = {
 
 togarashi.weaponMaterials = {
     wood: "togarashi.materials.wood",
-    copper: "togarashi.materials.copper"
+    copper: "togarashi.materials.copper",
+    iron: "togarashi.materials.iron",
+    titanium: "togarashi.materials.titanium",
+    silver: "togarashi.materials.silver",
+    gold: "togarashi.materials.gold",
+    platinum: "togarashi.materials.platinum",
+    jade: "togarashi.materials.jade",
+    diamond: "togarashi.materials.diamond",
+    mithril: "togarashi.materials.mithril",
+    bronze: "togarashi.materials.bronze",
+    steel: "togarashi.materials.steel",
+    black_steel: "togarashi.materials.black_steel",
+    damascus_steel: "togarashi.materials.damascus_steel",
+    electrum: "togarashi.materials.electrum",
+    black_wood: "togarashi.materials.black_wood",
+    orichalcos: "togarashi.materials.orichalcos",
+    adamantinium: "togarashi.materials.adamantinium"
 };
 
 togarashi.armorMaterials = {
     lether: "togarashi.materials.lether",
-    copper: "togarashi.materials.copper"
+    copper: "togarashi.materials.copper",
+    iron: "togarashi.materials.iron",
+    titanium: "togarashi.materials.titanium",
+    silver: "togarashi.materials.silver",
+    gold: "togarashi.materials.gold",
+    platinum: "togarashi.materials.platinum",
+    jade: "togarashi.materials.jade",
+    diamond: "togarashi.materials.diamond",
+    mithril: "togarashi.materials.mithril",
+    bronze: "togarashi.materials.bronze",
+    steel: "togarashi.materials.steel",
+    black_steel: "togarashi.materials.black_steel",
+    damascus_steel: "togarashi.materials.damascus_steel",
+    electrum: "togarashi.materials.electrum",
+    black_wood: "togarashi.materials.black_wood",
+    orichalcos: "togarashi.materials.orichalcos",
+    adamantinium: "togarashi.materials.adamantinium"
 };
 
 togarashi.damageTypes = {
--- module/data/materialStats.js
+++ module/data/materialStats.js
@@ -22,5 +22,125 @@ export const materialStats = {
         critical: 0,
         durability: 100,
         block: 1
+    },
+    titanium: {
+        damage: 4,
+        weight: 2,
+        accuracy: 2,
+        critical: 1,
+        durability: 2500,
+        block: 4
+    },
+    silver: {
+        damage: 1,
+        weight: 6,
+        accuracy: -2,
+        critical: 0,
+        durability: 50,
+        block: 1
+    },
+    gold: {
+        damage: 1,
+        weight: 7,
+        accuracy: -3,
+        critical: 0,
+        durability: 50,
+        block: 1
+    },
+    platinum: {
+        damage: -1,
+        weight: 7,
+        accuracy: -3,
+        critical: -1,
+        durability: 0,
+        block: 0
+    },
+    jade: {
+        damage: 4,
+        weight: 0,
+        accuracy: 1,
+        critical: 1,
+        durability: -100,
+        block: 3
+    },
+    diamond: {
+        damage: 5,
+        weight: 0,
+        accuracy: 1,
+        critical: 2,
+        durability: -700,
+        block: 2
+    },
+    mithril: {
+        damage: 5,
+        weight: 2,
+        accuracy: 1,
+        critical: 1,
+        durability: 3000,
+        block: 4
+    },
+    bronze: {
+        damage: 1,
+        weight: 7,
+        accuracy: -1,
+        critical: 0,
+        durability: 150,
+        block: 1
+    },
+    steel: {
+        damage: 2,
+        weight: 5,
+        accuracy: 0,
+        critical: 0,
+        durability: 1000,
+        block: 2
+    },
+    black_steel: {
+        damage: 2,
+        weight: 5,
+        accuracy: 0,
+        critical: 0,
+        durability: 2000,
+        block: 2
+    },
+    damascus_steel: {
+        damage: 3,
+        weight: 4,
+        accuracy: 1,
+        critical: 1,
+        durability: 1000,
+        block: 1
+    },
+    electrum: {
+        damage: 4,
+        weight: 8,
+        accuracy: -1,
+        critical: 0,
+        durability: 100,
+        block: 3
+    },
+    black_wood: {
+        damage: 2,
+        weight: 1,
+        accuracy: 1,
+        critical: 0,
+        durability: 500,
+        block: 2
+    },
+    orichalcos: {
+        damage: 6,
+        weight: 1,
+        accuracy: 2,
+        critical: 2,
+        durability: 600,
+        block: 5
+    },
+    adamantinium: {
+        damage: 7,
+        weight: 2,
+        accuracy: 1,
+        critical: 2,
+        durability: 5000,
+        block: 6
     }
 };
